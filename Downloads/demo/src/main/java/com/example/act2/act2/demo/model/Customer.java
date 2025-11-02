6b65feea4f4501456e809377deeb9cb5eaabfac0
Waeeeell <waelelk053@gmail.com>
2025-11-02 17:16:56 +0100

    Finalitazt

---
+++ Downloads/demo/src/main/java/com/example/act2/act2/demo/model/Customer.java
@@ -0,0 +1,78 @@
+package com.example.act2.act2.demo.model;
+
+import java.sql.Timestamp;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private String description;
+    private String email;
+    private String password;
+    private Timestamp ultimAcces;
+    private Timestamp dataCreated;
+    private Timestamp dataUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getUltimAcces() {
+        return ultimAcces;
+    }
+
+    public void setUltimAcces(Timestamp ultimAcces) {
+        this.ultimAcces = ultimAcces;
+    }
+
+    public Timestamp getDataCreated() {
+        return dataCreated;
+    }
+
+    public void setDataCreated(Timestamp dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+
+    public Timestamp getDataUpdated() {
+        return dataUpdated;
+    }
+
+    public void setDataUpdated(Timestamp dataUpdated) {
+        this.dataUpdated = dataUpdated;
+    }
+}
